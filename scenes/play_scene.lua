feb26ef40dac66e45cc707472023ead9dd2f9da0
4v0v <30574115+4v0v@users.noreply.github.com>
2021-02-08 18:29:32 +0100

    ...

---
+++ scenes/play_scene.lua
@@ -0,0 +1,11 @@
+Play_scene = Scene:extend('Play_scene')
+
+function Play_scene:new()
+	Play_scene.super.new(@)
+end
+
+function Play_scene:update(dt)
+	Play_scene.super.update(@, dt)
+
+	if pressed('escape') then change_scene_with_transition('menu') end
+end
